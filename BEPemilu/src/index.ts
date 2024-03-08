0756d1065fc63cad7517e369bab3dfc5f4cc6753
Ky2510 <rinaldirizqifauzi@gmail.com>
2024-03-08 14:32:44 +0700

    Cors implementation for fortend clear commit

--- BEPemilu/src/index.ts
+++ BEPemilu/src/index.ts
@@ -1,11 +1,17 @@
 import { AppDataSource } from "./data-source"
-import * as express from "express"
+import express from "express"
 import Route from "./routes"
+import cors from "cors"
 
 AppDataSource.initialize().then(async () => {
     const app = express()
     const port = 3000
 
+    var corsOptions = {
+        origin: "http://localhost:5173"
+    }
+
+    app.use(cors(corsOptions))
     app.use(express.json())
     app.use('/api/v1', Route)
 
